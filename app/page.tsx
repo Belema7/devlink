c1b1fe214d51857bef0d59456c8d2d2b9153a6c3
belema7 <belemagirma31@gmail.com>
2026-04-11 18:07:42 +0300

    feat: implement authentication with better-auth and add user model to Prisma schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/db";
 
 export default async function Home() {
   const posts = await prisma.post.findMany();
   return (
-    <main style={{ padding: 20 }}>
-      <h1>Posts</h1>
-      <ul>
+    <main className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Posts</h1>
+      <ul className="mt-4 space-y-4">
         {posts.map((p) => (
-          <li key={p.id}>
-            <strong>{p.title}</strong>
-            <p>{p.content}</p>
+          <li key={p.id} className="p-4 border rounded">
+            <strong className="text-lg">{p.title}</strong>
+            <p className="text-muted-foreground">{p.content}</p>
           </li>
         ))}
       </ul>
+      <Button  variant="secondary" >Click me</Button>
     </main>
   );
 }
