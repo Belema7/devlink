31ab4384b9372d0042c970c74f88681ec78291bb
belema7 <belemagirma31@gmail.com>
2026-04-12 15:11:20 +0300

    feat: remove Navbar component from DashboardLayout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from '@/components/layout/navbar'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Navbar />
       <div>
         <header>
           <h1>My Dashboard</h1>
